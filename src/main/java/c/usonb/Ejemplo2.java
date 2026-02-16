9f98a168f551f2b21f096f0713d15a2702f9ba6c
GSukiaG <gsukia@educa.madrid.org>
2026-02-16 17:26:48 +0100

    cambi en Ejemplo2

--- src/main/java/c/usonb/Ejemplo2.java
+++ src/main/java/c/usonb/Ejemplo2.java
@@ -6,6 +6,10 @@ public class Ejemplo2 {
 
 public static void main(String[] args) {
 Scanner entrada = new Scanner(System.in);
+
+System.out.println("Hola");
+
+
 System.out.println("Introduce tu nombre, por favor:");
 String nombre = entrada.nextLine();
 System.out.println("Hola " + nombre + "!");
